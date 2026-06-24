543679e7d238323aa79b336f6f467b9cd2e7b5e0
lightningund <lightningundies0826@gmail.com>
2026-06-24 16:00:30 -0500

    cleaned up the graph code a ton

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::thread;
 use std::sync::{mpsc};
 
 use eframe::egui;
-use egui::{Ui, Color32};
+use egui::{Ui, Color32, Pos2, Vec2, Rect};
 
 static MAX_HIST: usize = 100;
 static UPDATE_TIME: Duration = Duration::from_millis(200);
@@ -29,18 +29,35 @@ fn main() -> eframe::Result {
 }
 
 trait Arithmetic: Copy + Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> {}
+impl<T> Arithmetic for T where T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> {}
 
 fn map<T, U>(val: T, a_min: T, a_max: T, b_min: U, b_max: U) -> U
 where
-	T: Arithmetic + 'static + num_traits::AsPrimitive<U>,
+	T: Copy + num_traits::AsPrimitive<U>,
 	U: Arithmetic + 'static
 {
-	((val - a_min) / (a_max - a_min)).as_() * (b_max - b_min) + b_min
+	((val.as_() - a_min.as_()) / (a_max.as_() - a_min.as_())) * (b_max - b_min) + b_min
 }
 
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 struct Stats {
 	memory: (usize, usize), // Used and available
+}
+
+fn get_max_memory() -> Option<usize> {
+	let proc = Command::new("free")
+		.arg("-w")
+		.stdout(Stdio::piped())
+		.output().expect("Couldn't Create Thread")
+		.stdout;
+
+	if let Ok(output) = String::from_utf8(proc) {
+		let parts = output.split(&[' ', '\n']).filter(|s| !s.is_empty()).collect::<Vec<_>>();
+		let max = parts[8].parse();
+		return max.ok();
+	}
+
+	None
 }
 
 fn get_memory() -> Option<(usize, usize)> {
@@ -114,26 +131,21 @@ impl<T: Copy + Ord> History<T> {
 	}
 }
 
-impl<T: num_traits::AsPrimitive<f32> + Ord> History<T> {
+impl<T: Arithmetic + num_traits::AsPrimitive<f32> + Ord> History<T> {
 	// TODO: Make this return a response
 	fn draw(&self, ui: &mut Ui, stroke: egui::Stroke) {
-		let size = egui::Rect::from_min_size(egui::Pos2::new(0.0, 0.0), egui::Vec2::new(500.0, 200.0)).translate(ui.cursor().min.to_vec2());
+		let size = Rect::from_min_size(Pos2::new(0.0, 0.0), Vec2::new(500.0, 200.0)).translate(ui.cursor().min.to_vec2());
 		let painter = ui.painter_at(size);
 		ui.advance_cursor_after_rect(painter.clip_rect());
 
-		// println!("{:?}", painter.clip_rect());
-
 		painter.rect_stroke(painter.clip_rect(), 0, stroke, egui::StrokeKind::Inside);
 
-		let height_scale = 100.0 / self.max.as_();
-		let width_scale = 500.0 / self.hist.len() as f32;
-
-		let start = painter.clip_rect().min.to_vec2();
+		let Rect{ min: Pos2{x: ax, y: ay}, max: Pos2{x: bx, y: by} } = painter.clip_rect();
 
 		painter.line(self.hist.iter().enumerate().map(|(idx, v)| {
-			let scaled_x: f32 = idx as f32 * width_scale;
-			let scaled_y: f32 = (*v).as_() * height_scale;
-			egui::Pos2::new(scaled_x, scaled_y) + start
+			let scaled_x: f32 = map(idx, 0, self.hist.len(), ax, bx);
+			let scaled_y: f32 = map(*v, self.min, self.max, ay, by);
+			Pos2::new(scaled_x, scaled_y)
 		}).collect(), stroke);
 	}
 }
@@ -158,19 +170,9 @@ impl MyApp {
 			avail_mem: Default::default(),
 		};
 
-		let proc = Command::new("free")
-			.arg("-w")
-			.stdout(Stdio::piped())
-			.output().expect("Couldn't Create Thread")
-			.stdout;
-
-		if let Ok(output) = String::from_utf8(proc) {
-			let parts = output.split(&[' ', '\n']).filter(|s| !s.is_empty()).collect::<Vec<_>>();
-			let max = parts[8].parse();
-			if let Ok(max) = max {
-				obj.used_mem.max = max;
-				obj.avail_mem.max = max;
-			}
+		if let Some(max) = get_max_memory() {
+			obj.used_mem.max = max;
+			obj.avail_mem.max = max;
 		}
 
 		obj
