c9ae9b946331ea7aac0276dd8669bc49b978f8b3
lightningund <lightningundies0826@gmail.com>
2026-06-27 16:43:36 -0500

    converted from message passing to shared state

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::ops::{Sub, Add, Mul, Div};
 use std::process::{Command, Stdio};
 use std::time::{Duration, Instant};
 use std::thread;
-use std::sync::{mpsc, RwLock};
+use std::sync::{mpsc, RwLock, Arc};
 
 use eframe::egui;
 use egui::{Ui, Color32, Pos2, Rect};
@@ -39,125 +39,6 @@ where
 {
 	use num_traits::AsPrimitive;
 	(((val.as_() - a_min.as_()) / (a_max.as_() - a_min.as_())) * (b_max.as_() - b_min.as_()) + b_min.as_()).as_()
-}
-
-#[derive(Clone, Copy, Debug, Default, PartialEq)]
-struct Stats {
-	memory: Option<usize>, // Used and available
-	cpu_usage: Option<f32>,
-}
-
-fn split(src: &str) -> impl Iterator<Item = &str> {
-	src.split(&[' ', '\t', '\n', '\r']).filter(|s| !s.is_empty())
-}
-
-fn run_cmd(cmd: &str, args: &[&str]) -> Option<Vec<String>> {
-	let proc = Command::new(cmd)
-		.args(args)
-		.stdout(Stdio::piped())
-		.output().expect("Couldn't Create Thread")
-		.stdout;
-
-	String::from_utf8(proc).map(|output| split(&output).map(|s| s.to_string()).collect()).ok()
-}
-
-fn get_max_memory() -> Option<usize> {
-	if let Some(parts) = run_cmd("free", &["-w"]) {
-		let max = parts[8].parse();
-		return max.ok();
-	}
-
-	None
-}
-
-fn get_memory() -> Option<usize> {
-	if let Some(parts) = run_cmd("free", &["-w"]) {
-		let used = parts[9].parse();
-		return used.ok();
-	}
-
-	None
-}
-
-#[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
-struct CpuCounts {
-	user: usize,
-	nice: usize,
-	system: usize,
-	idle: usize,
-}
-
-impl std::ops::Sub for CpuCounts {
-	type Output = Self;
-
-	fn sub(self, rhs: Self) -> Self::Output {
-		Self {
-			user: self.user - rhs.user,
-			nice: self.nice - rhs.nice,
-			system: self.system - rhs.system,
-			idle: self.idle - rhs.idle,
-		}
-	}
-}
-
-impl CpuCounts {
-	fn total(&self) -> usize {
-		self.user + self.nice + self.system + self.idle
-	}
-}
-
-static CPU_STATE: RwLock<CpuCounts> = RwLock::new(CpuCounts {
-	user: 0,
-	nice: 0,
-	system: 0,
-	idle: 0,
-});
-
-fn get_cpu_usage() -> Option<f32> {
-	// Run `head` on /proc/stat
-	let parts = run_cmd("head", &["--lines", "1", "/proc/stat"])?;
-
-	// Read /proc/stat directly
-	// let procfile = std::fs::File::open("/proc/stat").expect("Couldn't open file");
-	// let reader = BufReader::new(procfile);
-	// for line in reader.lines().take(1) {
-	// 	if let Ok(line) = line {
-	// 		println!("{}", line);
-	// 	}
-	// }
-
-	// Parse out the different stats overall
-	let user: usize = parts[1].parse().expect("Not a number?");
-	let nice: usize = parts[2].parse().expect("Not a number?");
-	let system: usize = parts[3].parse().expect("Not a number?");
-	let idle: usize = parts[4].parse().expect("Not a number?");
-
-	let curr_count = CpuCounts { user, nice, system, idle };
-
-	// Get the difference from the previous state
-	let counts = CPU_STATE.read().ok()?;
-	let diff = curr_count - *counts;
-	drop(counts);
-
-	// Update the current state
-	let mut state = CPU_STATE.write().ok()?;
-	*state = curr_count;
-
-	// Calculate the percentage of time idle
-	let total = diff.total();
-	let idle_p = (diff.idle as f32) / (total as f32);
-	Some((1.0 - idle_p) * 100.0)
-}
-
-fn updater(tx: mpsc::Sender<Stats>) {
-	loop {
-		let _ = tx.send(Stats {
-			memory: get_memory(),
-			cpu_usage: get_cpu_usage(),
-		});
-
-		thread::sleep(UPDATE_TIME);
-	}
 }
 
 #[derive(Clone, Debug)]
@@ -266,12 +147,137 @@ impl<T: ToString + num_traits::AsPrimitive<f32>> History<T> {
 	}
 }
 
-#[derive(Debug)]
-struct MyApp {
-	rx: mpsc::Receiver<Stats>,
-	// next_update: Instant,
+#[derive(Clone, Debug, Default)]
+struct Stats {
 	used_mem: History<usize>,
 	cpu_usage: History<f32>,
+}
+
+fn split(src: &str) -> impl Iterator<Item = &str> {
+	src.split(&[' ', '\t', '\n', '\r']).filter(|s| !s.is_empty())
+}
+
+fn run_cmd(cmd: &str, args: &[&str]) -> Option<Vec<String>> {
+	let proc = Command::new(cmd)
+		.args(args)
+		.stdout(Stdio::piped())
+		.output().expect("Couldn't Create Thread")
+		.stdout;
+
+	String::from_utf8(proc).map(|output| split(&output).map(|s| s.to_string()).collect()).ok()
+}
+
+fn get_max_memory() -> Option<usize> {
+	if let Some(parts) = run_cmd("free", &["-w"]) {
+		let max = parts[8].parse();
+		return max.ok();
+	}
+
+	None
+}
+
+fn get_memory() -> Option<usize> {
+	if let Some(parts) = run_cmd("free", &["-w"]) {
+		let used = parts[9].parse();
+		return used.ok();
+	}
+
+	None
+}
+
+#[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
+struct CpuCounts {
+	user: usize,
+	nice: usize,
+	system: usize,
+	idle: usize,
+}
+
+impl std::ops::Sub for CpuCounts {
+	type Output = Self;
+
+	fn sub(self, rhs: Self) -> Self::Output {
+		Self {
+			user: self.user - rhs.user,
+			nice: self.nice - rhs.nice,
+			system: self.system - rhs.system,
+			idle: self.idle - rhs.idle,
+		}
+	}
+}
+
+impl CpuCounts {
+	fn total(&self) -> usize {
+		self.user + self.nice + self.system + self.idle
+	}
+}
+
+static CPU_STATE: RwLock<CpuCounts> = RwLock::new(CpuCounts {
+	user: 0,
+	nice: 0,
+	system: 0,
+	idle: 0,
+});
+
+fn get_cpu_usage() -> Option<f32> {
+	// Run `head` on /proc/stat
+	let parts = run_cmd("head", &["--lines", "1", "/proc/stat"])?;
+
+	// Read /proc/stat directly
+	// let procfile = std::fs::File::open("/proc/stat").expect("Couldn't open file");
+	// let reader = BufReader::new(procfile);
+	// for line in reader.lines().take(1) {
+	// 	if let Ok(line) = line {
+	// 		println!("{}", line);
+	// 	}
+	// }
+
+	// Parse out the different stats overall
+	let user: usize = parts[1].parse().expect("Not a number?");
+	let nice: usize = parts[2].parse().expect("Not a number?");
+	let system: usize = parts[3].parse().expect("Not a number?");
+	let idle: usize = parts[4].parse().expect("Not a number?");
+
+	let curr_count = CpuCounts { user, nice, system, idle };
+
+	// Get the difference from the previous state
+	let counts = CPU_STATE.read().ok()?;
+	let diff = curr_count - *counts;
+	drop(counts);
+
+	// Update the current state
+	let mut state = CPU_STATE.write().ok()?;
+	*state = curr_count;
+
+	// Calculate the percentage of time idle
+	let total = diff.total();
+	let idle_p = (diff.idle as f32) / (total as f32);
+	Some((1.0 - idle_p) * 100.0)
+}
+
+fn updater(stats: Arc<RwLock<Stats>>) {
+	loop {
+		if let Ok(mut stats) = stats.write() {
+			if let Some(mem) = get_memory() {
+				stats.used_mem.add(mem);
+			}
+
+			if let Some(cpu) = get_cpu_usage() {
+				stats.cpu_usage.add_unbounded(cpu);
+			}
+		}
+
+		thread::sleep(UPDATE_TIME);
+	}
+}
+
+#[derive(Debug)]
+struct MyApp {
+	stats: Arc<RwLock<Stats>>,
+	// rx: mpsc::Receiver<Stats>,
+	// next_update: Instant,
+	// used_mem: History<usize>,
+	// cpu_usage: History<f32>,
 }
 
 impl MyApp {
@@ -280,43 +286,43 @@ impl MyApp {
 	/// Not labelled as default because it spawns processes and does stuff
 	/// which doesn't really feel like what you would expect from a default function
 	fn new() -> Self {
-		let (tx, rx) = mpsc::channel();
-		thread::spawn(move || {
-			updater(tx);
-		});
-
-		let mut obj = Self {
-			rx,
-			// next_update: Instant::now(),
+		let stats = Arc::new(RwLock::new(Stats {
 			used_mem: Default::default(),
 			cpu_usage: Default::default(),
-		};
+		}));
 
-		obj.used_mem.name = "Used Memory".to_string();
-		obj.cpu_usage.name = "CPU Usage".to_string();
+		if let Ok(mut stats) = stats.write() {
+			stats.used_mem.name = "Used Memory".to_string();
+			stats.cpu_usage.name = "CPU Usage".to_string();
 
-		if let Some(max) = get_max_memory() {
-			obj.used_mem.max = max;
+			if let Some(max) = get_max_memory() {
+				stats.used_mem.max = max;
+			}
+
+			stats.cpu_usage.max = 100.0;
 		}
 
-		obj.cpu_usage.max = 100.0;
+		let thread_stats = Arc::clone(&stats);
+		thread::spawn(move || {
+			updater(thread_stats);
+		});
 
-		obj
+		Self { stats }
 	}
 }
 
 impl eframe::App for MyApp {
 	// This is called every time the screen updates
 	fn ui(&mut self, ui: &mut Ui, _frame: &mut eframe::Frame) {
-		if let Ok(resp) = self.rx.try_recv() {
-			if let Some(mem) = resp.memory {
-				self.used_mem.add(mem);
-			}
+		// if let Ok(resp) = self.rx.try_recv() {
+		// 	if let Some(mem) = resp.memory {
+		// 		self.used_mem.add(mem);
+		// 	}
 
-			if let Some(cpu) = resp.cpu_usage {
-				self.cpu_usage.add_unbounded(cpu);
-			}
-		}
+		// 	if let Some(cpu) = resp.cpu_usage {
+		// 		self.cpu_usage.add_unbounded(cpu);
+		// 	}
+		// }
 
 		// let now = Instant::now();
 		// if now > self.next_update {
@@ -330,11 +336,14 @@ impl eframe::App for MyApp {
 		// 	}
 		// }
 
-		self.used_mem.draw(ui, egui::Stroke::new(1.0, Color32::WHITE));
-		self.cpu_usage.draw(ui, egui::Stroke::new(1.0, Color32::GREEN));
+		if let Ok(stats) = self.stats.read() {
+			stats.used_mem.draw(ui, egui::Stroke::new(1.0, Color32::WHITE));
+			stats.cpu_usage.draw(ui, egui::Stroke::new(1.0, Color32::GREEN));
+		}
 
 		// Make sure it draws again
-		ui.request_repaint_after(SCREEN_UPDATE);
+		ui.request_repaint_after(UPDATE_TIME);
+		// ui.request_repaint_after(SCREEN_UPDATE);
 		// ui.request_repaint();
 	}
 }
